ce4b942ee9a15f7bb1ade7cf5393ebe13871f3a2
Andres Guerra <andresfg13789@gmail.com>
2024-03-07 00:03:31 -0500

    java

--- java/Curso Java/Introduccion a java/HolaMundo.java
+++ java/Curso Java/Introduccion a java/HolaMundo.java
@@ -3,7 +3,9 @@
  */
 public class HolaMundo {
 
-    public static void main( String args[] ) {
-        System.out.println("Hola Mundo");
+    public static void main(String args[]) {
+        int n = 20;
+        int m = 10;
+        System.out.println("Hola Mundo" + (n + m));
     }
 }
