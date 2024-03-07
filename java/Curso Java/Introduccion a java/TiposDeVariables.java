118dc54f82ea2e2938445457c8b5509556a662d2
Andres Guerra <andresfg13789@gmail.com>
2024-03-07 18:47:31 -0500

    aula en la nube

---
+++ java/Curso Java/Introduccion a java/TiposDeVariables.java
@@ -0,0 +1,12 @@
+public class TiposDeVariables {
+    
+    public static void main(String[] args){
+        byte num1 = 0x10;
+        int num2 = 0xff;
+        byte num3 = (byte)0xff;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
