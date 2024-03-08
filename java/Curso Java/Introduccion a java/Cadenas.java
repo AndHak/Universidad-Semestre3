49575eb371ee1f84286067041cdee2d8807aadb7
Andres Guerra <andresfg13789@gmail.com>
2024-03-07 20:07:29 -0500

    aula en la nube

---
+++ java/Curso Java/Introduccion a java/Cadenas.java
@@ -0,0 +1,18 @@
+public class Cadenas {
+
+    public static void main(String[] args) {
+        
+        String cadena = "curso Java";
+
+        System.out.println(cadena.length());
+
+        int longitud = cadena.length();
+
+        System.out.println(cadena + " tiene " + longitud + " letras.");
+
+        char primera_letra = cadena.charAt(0);
+
+        System.out.println("La primera letra es: " + primera_letra);
+    }
+    
+}
---
+++ java/Curso Java/Introduccion a java/Unicode.java
@@ -0,0 +1,8 @@
+public class Unicode {
+    public static void main(String[] args) {
+
+        char letra1 = '\u221e';
+
+        System.out.println(letra1);
+    }
+}
