e2326ec1c43594338689b05fea0f5457a0c9cfb0
Andres Guerra <andresfg13789@gmail.com>
2024-03-31 01:55:08 -0500

    base

---
+++ python/programacion 3/base.py
@@ -0,0 +1,33 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+import sys
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Nombre app")
+
+        incon_image = r"direccion del png"
+        self.icon_window = QPixmap(incon_image)
+        self.setWindowIcon(self.icon_window)
+
+        self.resize(QSize(800, 800))
+
+        self.root_layout = QVBoxLayout()
+
+        self.frame_principal = QFrame()
+        self.frame_principal.setStyleSheet("background: #252938")
+
+        self.root_layout.addWidget(self.frame_principal, 100)
+
+        self.main_widget = QWidget()
+        self.main_widget.setLayout(self.root_layout)
+        self.setCentralWidget(self.main_widget)
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exit(app.exec())
